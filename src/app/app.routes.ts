315127721c7fc67fbb35f3a9b368d45640dee9da
sebastianfabrizio <sebasotaradiaz@gmail.com>
2024-06-01 14:14:05 -0500

    Diseñando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FundamentosComponent } from './fundamentos/fundamentos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"fundamentos", component: FundamentosComponent}
+];
